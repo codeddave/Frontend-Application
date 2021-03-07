c320275e51a772269dad5774e4bce20306fd013d
Dvaid Adeleye <drizzzydave@gmail.com>
2021-03-07 22:48:04 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Header from "./component/Header/Header";
+import Main from "./component/Main/Main";
+import Product from "./component/Product/Product";
+import { createGlobalStyle } from "styled-components";
+import Navigation, { NavContent } from "./component/Navigation/Navigation";
+import { useEffect, useState } from "react";
+const GlobalStyle = createGlobalStyle`
+ html, body {
+    margin: 0;
+    padding: 0;
+    font-family: Open-Sans, Helvetica, Sans-Serif;
+  }
+  
+`;
 function App() {
+  const [width, setwidth] = useState();
+  useEffect(() => {
+    function handleResize() {
+      setwidth(window.innerWidth);
+      window.addEventListener("resize", handleResize);
+    }
+
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <GlobalStyle />
+      <div className="app">
+        <Header />
+        <div className="content-container">
+          {width > 768 ? <NavContent /> : null}
+          <Switch>
+            <Route exact path="/" component={Main} />
+            <Route exact path="/product" component={Product} />
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import Navigation from "../Navigation/Navigation";
+import { Icon, InlineIcon } from "@iconify/react";
+import menuIcon from "@iconify-icons/mdi/menu";
+
+import "./Header.scss";
+
+function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openSidebar = () => {
+    setIsOpen(true);
+  };
+  const closeSidebar = () => {
+    setIsOpen(false);
+  };
+  return (
+    <nav>
+      <div>
+        <Icon icon={menuIcon} onClick={openSidebar} className="menu-icon" />
+        <img src={"https://img.innoloft.de/logo.svg"} alt="logo" />
+      </div>
+      <div className="navs">
+        <Navigation show={isOpen} onClick={closeSidebar} />
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
