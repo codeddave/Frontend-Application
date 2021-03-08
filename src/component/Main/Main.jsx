9386a4e92a41b8770dd0b0bed5845291fda1de65
Dvaid Adeleye <drizzzydave@gmail.com>
2021-03-08 08:09:26 +0100

    code clean up

--- src/component/Main/Main.jsx
+++ src/component/Main/Main.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-
+import "./Main.scss";
 function Main() {
   return (
-    <div>
-      <h1>Hello from main</h1>
+    <div className="main">
+      <h2>
+        This is the main page, navigate to the product route to view product.
+      </h2>
     </div>
   );
 }
